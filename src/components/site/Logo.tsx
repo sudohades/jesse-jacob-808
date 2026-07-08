459f1226102aeda92b217da5fb0ddee5c6afdcfe
sudohades <jkiprutto63003@anu.ac.ke>
2026-07-08 17:24:28 +0300

    fix(logo): Small tracking changes to logo

--- src/components/site/Logo.tsx
+++ src/components/site/Logo.tsx
@@ -6,7 +6,7 @@ export function Logo({ className }: { className?: string }) {
       <img src="/logo-24.png" alt="Logo" className="h-8 w-8 rounded-full" />
       <div className={cn("flex flex-col items-start gap-0.5 text-xs font-medium tracking-[0.22em] uppercase", className)}>
         <span className="text-sm font-medium tracking-[0.22em] uppercase">S U D O . H A D E S</span>
-      <span className="text-sm font-medium tracking-[0.620em] uppercase text-muted-foreground">Jesse Jacob</span>
+      <span className="text-sm font-medium tracking-[0.59em] uppercase text-muted-foreground">Jesse Jacob</span>
       </div>
     </div>
   );
